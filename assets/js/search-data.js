d263871103306b9b005d2978c7168ef817946689
amyawu <68081958+amyawu@users.noreply.github.com>
2025-09-02 21:25:32 +0000

    Deploying to gh-pages from @ amyawu/amyawu.github.io@8485a13382322309446a0508e5af04943eaf3de4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Taught Information and Databases Systems 1, Engineering Design and Society, Research Fundamentals",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
